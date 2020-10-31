0a38cdd7bdc91746506696d0e5f101facf2f2277
Deep Muni <deep.muni94@gmail.com>
2020-10-31 20:01:36 -0300

    Added Input Box

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
+import React from "react";
+import SearchIcon from '@material-ui/icons/Search';
 
 function App() {
-  return (
-    <div className="app">
-    </div>
-  );
+    return (
+        <div className="app">
+            <h1>AeroSoar</h1>
+            <div className="app__section">
+                <div className="app__caption">
+                    Please enter your ticket number
+                </div>
+                <div className="app__search">
+                    <input type="text" className="app__searchBar" />
+                    <SearchIcon className="app__searchIcon"
+                                style={{fontSize: "32px", width: "40px"}}/>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
